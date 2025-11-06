68383145f3698fda1491e436e6505c87db95c674
CasualWanderer20XX <dgxrmm2000@protonmail.com>
2025-11-05 19:12:24 -0800

    Added a new setting.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ topicLinks.forEach(topic => {
     const topicRegex = /[a-zA-z]+(?=\-link)/g;
     topic.addEventListener("click", (e) => {
         quizType = e.target.id.match(topicRegex)[0];
-        setTitle();
         setQuizType();
     })
 });
 
 const setQuizType = () => {
     localStorage.setItem("quizType", JSON.stringify(quizType));
+    console.log(quizType);
 }
--- src/quiz-init.js
+++ src/quiz-init.js
@@ -1,9 +1,24 @@
 const quizTitle = document.getElementById('quiz-title');
 
 const topicLinks = [...document.getElementsByClassName("topic-link")];
+const numQuestions = document.getElementById("num-questions");
+
+const questionSetTable = {
+    "algebra": "../json/algebraQuestions.json",
+    "vocabulary": "../json/vocabQuestions.json",
+    "history": "../json/historyQuestions.json"
+}
+
+const setNumQuestions = async (type) => {
+    console.log("CHECK");
+    const res = await fetch(questionSetTable[type]);
+    const data = await res.json();
+    numQuestions.setAttribute("max", data.length);
+    console.log(numQuestions.getAttribute("max"));
+}
 
 let quizType = JSON.parse(localStorage.getItem('quizType'));
-
+setNumQuestions(quizType);
 const setTitle = () => {
     quizTitle.innerText = `${quizType[0].toUpperCase() + quizType.slice(1)} Quiz`;
 }
@@ -14,6 +29,7 @@ topicLinks.forEach(topic => {
         quizType = e.target.id.match(topicRegex)[0];
         setTitle();
         setQuizType();
+        setNumQuestions(quizType);
     })
 });
 
